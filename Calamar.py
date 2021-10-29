cdcdd36cd693bee3ebf6a50c895fd9fb7beaf29a
Dani <gohandeeo@gmail.com>
2021-10-28 20:58:28 -0500

    Calamar

---
+++ Calamar.py
@@ -0,0 +1,78 @@
+import random
+PlayerMarbles = 10
+CPUMarbles = 10
+Even = False
+
+def CPUWins():
+    print(f"El Rival Habia apostado {CPUBet} canicas.")
+    print(f"Tú apostaste {PlayerBet} canicas.")
+    print(f" - Has perdido {PlayerBet} canicas")
+    print("")
+    print("=========================================")
+
+def PlayerWins():
+    print(f"El Rival Habia apostado {CPUBet} canicas.")
+    print(f"Tú apostaste {PlayerBet} canicas.")
+    print(f" - Has Ganado {CPUBet} canicas")
+    print("")
+    print("=========================================")
+
+def EndGame():
+    if PlayerMarbles == 20:
+        Game = False
+        return Game
+    if CPUMarbles == 20:
+        Game = False
+        return Game
+
+print(f"Tienes {PlayerMarbles} Canicas.")
+Game = True
+while Game:
+    ### Turno Jugador ###
+    print("¿Cuantas Canicas quieres apostar?")
+    PlayerBet = int(input())
+    print("")
+    if PlayerBet % 2 == 0:    #True = Par
+        Even = True           #False = Inpar
+    else:
+        Even = False
+
+    CPUBet = random.randint(1,CPUMarbles)
+    CPUGuess = bool(random.getrandbits(1))
+    if CPUGuess == True:
+        print("El Rival apostó por Par.")
+        if CPUGuess == Even:
+            CPUWins()
+            PlayerMarbles -= PlayerBet
+            CPUMarbles += PlayerBet
+        if CPUGuess != Even:
+            PlayerWins()
+            CPUMarbles -= CPUBet
+            PlayerMarbles += CPUBet
+    else:
+        print("El Rival apostó por Inpar.")
+        if CPUGuess == Even:
+            CPUWins()
+            PlayerMarbles -= PlayerBet
+            CPUMarbles += PlayerBet
+        if CPUGuess != Even:
+            PlayerWins()
+            CPUMarbles -= CPUBet
+            PlayerMarbles += CPUBet
+
+    print("")
+    print(f"Tienes {PlayerMarbles} Canicas.")
+    
+
+    if PlayerMarbles == 20:
+        Game = False
+        print("")
+        print("Ganaste.")
+        print("")
+        print("=========================================")
+    if CPUMarbles == 20:
+        Game = False
+        print("")
+        print("Perdiste.")
+        print("")
+        print("=========================================")
